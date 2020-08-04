07b7e0079aef000209df06a3f64c95c2f3f57af2
esh0504 <dmatmdgh49@gmail.com>
2020-08-04 16:44:47 +0900

    setting prop

--- ios/local_kevitTests/local_kevitTests.m
+++ ios/local_kevitTests/local_kevitTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface local_kevitTests : XCTestCase
+@interface git_kevitTests : XCTestCase
 
 @end
 
-@implementation local_kevitTests
+@implementation git_kevitTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
